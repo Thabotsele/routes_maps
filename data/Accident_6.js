bbd873bdc4f5f8d140e7fd98cabfc94d147cf937
Thabotsele <137162359+Thabotsele@users.noreply.github.com>
2025-05-22 16:33:40 +0200

    Add files via upload

---
+++ data/Accident_6.js
@@ -0,0 +1,17 @@
+var json_Accident_6 = {
+"type": "FeatureCollection",
+"name": "Accident_6",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": 1.0, "date_repor": "2025-06-03", "severity": "Meduim" }, "geometry": { "type": "Point", "coordinates": [ 18.448560916558669, -33.976929921922576 ] } },
+{ "type": "Feature", "properties": { "id": 2.0, "date_repor": "2025-05-31", "severity": "Medium" }, "geometry": { "type": "Point", "coordinates": [ 18.583533662956732, -33.921711454042061 ] } },
+{ "type": "Feature", "properties": { "id": 3.0, "date_repor": "2025-06-30", "severity": "High" }, "geometry": { "type": "Point", "coordinates": [ 18.499613221590796, -33.877860067789577 ] } },
+{ "type": "Feature", "properties": { "id": 4.0, "date_repor": "2025-06-18", "severity": "Low" }, "geometry": { "type": "Point", "coordinates": [ 18.686819646552816, -33.955940191194017 ] } },
+{ "type": "Feature", "properties": { "id": 5.0, "date_repor": "2025-06-01", "severity": "Medium" }, "geometry": { "type": "Point", "coordinates": [ 18.591705266990459, -34.057190471376998 ] } },
+{ "type": "Feature", "properties": { "id": 6.0, "date_repor": "2025-06-20", "severity": "Low" }, "geometry": { "type": "Point", "coordinates": [ 18.483947062074868, -34.067895450994889 ] } },
+{ "type": "Feature", "properties": { "id": 7.0, "date_repor": "2025-07-01", "severity": "Low" }, "geometry": { "type": "Point", "coordinates": [ 18.51007424410988, -33.592356847015353 ] } },
+{ "type": "Feature", "properties": { "id": 8.0, "date_repor": "2025-06-12", "severity": "Medium" }, "geometry": { "type": "Point", "coordinates": [ 18.606527144886613, -33.681682251697765 ] } },
+{ "type": "Feature", "properties": { "id": 9.0, "date_repor": "2025-06-17", "severity": "High" }, "geometry": { "type": "Point", "coordinates": [ 18.595692630720183, -33.838682777582939 ] } },
+{ "type": "Feature", "properties": { "id": 10.0, "date_repor": "2025-06-30", "severity": "Low" }, "geometry": { "type": "Point", "coordinates": [ 18.953186904332185, -34.151595871541964 ] } }
+]
+}
---
+++ data/Potholes_5.js
@@ -0,0 +1,13 @@
+var json_Potholes_5 = {
+"type": "FeatureCollection",
+"name": "Potholes_5",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": 1.0, "date_repor": "2025-03-06", "notes": "Small surface crack" }, "geometry": { "type": "Point", "coordinates": [ 18.567014824369732, -33.651413984092642 ] } },
+{ "type": "Feature", "properties": { "id": 2.0, "date_repor": "2025-04-08", "notes": "Medium pothole in right lane" }, "geometry": { "type": "Point", "coordinates": [ 18.874836207272345, -34.116551847772335 ] } },
+{ "type": "Feature", "properties": { "id": 3.0, "date_repor": "2025-04-30", "notes": "Large pothole, dangerous" }, "geometry": { "type": "Point", "coordinates": [ 18.420820521071814, -34.238424504976749 ] } },
+{ "type": "Feature", "properties": { "id": 4.0, "date_repor": "2025-02-13", "notes": "Light wear, not urgent" }, "geometry": { "type": "Point", "coordinates": [ 18.582925458593603, -33.98198013947183 ] } },
+{ "type": "Feature", "properties": { "id": 5.0, "date_repor": "2025-05-10", "notes": "Repaired last year" }, "geometry": { "type": "Point", "coordinates": [ 18.704266192401708, -33.750842852181421 ] } },
+{ "type": "Feature", "properties": { "id": 6.0, "date_repor": "2025-04-24", "notes": "Needs monitoring" }, "geometry": { "type": "Point", "coordinates": [ 18.473626969054195, -33.580465381049962 ] } }
+]
+}
